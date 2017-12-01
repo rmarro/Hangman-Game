6d91426bc3f8e8bc8559c4de0d79bbc1ed8bf731
rmarro <rmarro47@gmail.com>
2017-11-30 21:58:45 -0600

    reorganized, fixed scopes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -22,85 +22,111 @@
 
 
 // Set counter and score
-var remaining = 10;
-document.getElementById("remaining").innerHTML = remaining;
-
 var score = 0;
-document.getElementById("wins").innerHTML = score;
-
 // Create list of words and letters
-var wordList = ["dog", "hedgehog", "big bird"];
+var wordList = ["dog", "bird", "hedgehog", "calico cat"];
 
 var allowedLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
-
-// Empty array for guessed letters
+document.getElementById("wins").innerHTML = score;
+var remaining = 10;
 var alreadyGuessed = [];
+var chosenWord;
+var progressWord;
+var arrayWord;
+window.onload = reset();
 
-// Computer randomly selects a word
-var chosenWord = wordList[Math.floor(Math.random() * wordList.length)];
+function reset() {
 
-// Change the word into an array of letters
-var arrayWord = chosenWord.split("");
-console.log(arrayWord);
+	remaining = 10;
+	document.getElementById("remaining").innerHTML = remaining;
 
-// Create blanks that will be filled; fill in any spaces
-var progressWord = [];
+	// Empty array for guessed letters
+	alreadyGuessed = [];
+	document.getElementById("guessed").innerHTML = alreadyGuessed;
 
-for (var i = 0; i < arrayWord.length; i++) {
-	if (arrayWord[i] === " ") {
-    	progressWord.push("&nbsp");
-  	}
-  	else {
-    	progressWord.push("_");
-  	}
+	// Computer randomly selects a word
+	chosenWord = wordList[Math.floor(Math.random() * wordList.length)];
+
+
+	// Change the word into an array of letters
+	arrayWord = chosenWord.split("");
+	console.log(arrayWord);
+
+	// Create blanks that will be filled; fill in any spaces
+	progressWord = [];
+
+	for (var i = 0; i < arrayWord.length; i++) {
+		if (arrayWord[i] === " ") {
+	    	progressWord.push("&nbsp");
+	  	}
+	  	else {
+	    	progressWord.push("_");
+	  	}
+	}
+	// Create display with spaces instead of commas
+	var displayWord = progressWord.join(" ")
+	document.getElementById("display").innerHTML = displayWord;
 }
 
-// Create display with spaces instead of commas
-var displayWord = progressWord.join(" ")
-document.getElementById("display").innerHTML = displayWord;
 
-// When key is pressed, save that as guessed letter
-document.onkeyup = function(event) {
-	var userGuess = event.key;
-  
-  // Only if pressed key is an allowed letter
-  if (allowedLetters.indexOf(userGuess) != -1) {
-    	// Check if guessed letter is in the word
-    	// If yes
-    	if (arrayWord.indexOf(userGuess) != -1) { 
-    	  // Replace each correct letter in progress word
-    	  for (i=0; i < arrayWord.length; i++) {
-       		if (userGuess === arrayWord[i]) {
-          		progressWord.splice(i, 1, userGuess);
-          		// Get rid of commas for the display word
-          		displayWord = progressWord.join(" ");
-          		document.getElementById("display").innerHTML =                 displayWord;
-          		// If full word is done, increase score
-          		if (progressWord.indexOf("_") === -1) {
-            		score++;
-            		document.getElementById("wins").innerHTML = score;
-          		}
-        		}
-      	}
-    	}
 
-    	// If no
-    	else {
-      	// If not already in alreadyGuessed array, add it
-      	if (alreadyGuessed.indexOf(userGuess) === -1) {
-        		alreadyGuessed.push(userGuess);
-        		// Get rid of commas for display guesses and display it
-        		var displayAlreadyGuessed = alreadyGuessed.join(" ");
-        		document.getElementById("guessed").innerHTML = displayAlreadyGuessed;
-        		// Subtract one from counter and display it
-        		remaining--;
-        		document.getElementById("remaining").innerHTML = remaining;
-        		// If run out of guesses
-        		if (remaining === 0) {
-        			document.getElementById("remaining").innerHTML = "Out of guesses!"
-        		}
-      	}
-    	}
-  	}
-}
+	// When key is pressed, save that as guessed letter
+	document.onkeyup = function(event) {
+		var userGuess = event.key;
+	  
+	  // Only if pressed key is an allowed letter
+	  if (allowedLetters.indexOf(userGuess) != -1) {
+	    	// Check if guessed letter is in the word
+	    	// If yes
+	    	if (arrayWord.indexOf(userGuess) != -1) { 
+	    	  	// Replace each correct letter in progress word
+	    	  	for (j = 0; j < arrayWord.length; j++) {
+	       		if (userGuess === arrayWord[j]) {
+	          		progressWord.splice(j, 1, userGuess);
+	          		// Get rid of commas for the display word
+	          		displayWord = progressWord.join(" ");
+	          		document.getElementById("display").innerHTML = displayWord;
+	          		// If full word is done, increase score
+	          		if (progressWord.indexOf("_") === -1) {
+	            		score++;
+	            		document.getElementById("wins").innerHTML = score;
+	          		}
+	        		}
+	      	}
+	    	}
+
+	    	// If no
+	    	else {
+	      	// If not already in alreadyGuessed array, add it
+	      	if (alreadyGuessed.indexOf(userGuess) === -1) {
+	        		alreadyGuessed.push(userGuess);
+	        		// Get rid of commas for display guesses and display it
+	        		var displayAlreadyGuessed = alreadyGuessed.join(" ");
+	        		document.getElementById("guessed").innerHTML = displayAlreadyGuessed;
+	        		// Subtract one from counter and display it
+	        		remaining--;
+	        		document.getElementById("remaining").innerHTML = remaining;
+	        		// If run out of guesses
+	        		if (remaining === 0) {
+	        			document.getElementById("remaining").innerHTML = "Out of guesses!";
+	        		}
+	      	}
+	    	}
+	  	}
+	}
+
+
+
+
+
+// TO DO:
+
+// If guess full word, show pic/play song/etc
+
+// If win or lose, cannot guess anymore
+
+// DONE (??)
+// If win or lose, randomly choose new word
+// If win or lose, reset guesses remaining
+// If win or lose, reset already guessed list
+// Make a "Play again" button that does all this ^^ ??
